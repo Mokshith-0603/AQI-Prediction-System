5bc7e56dd907e724472c7b84fbe63fcf57c1bbf0
SiriVarshini04 <sirivarshini.pallamreddy@gmail.com>
2026-02-09 20:09:14 +0530

    Fix Indentation Error

--- app/streamlit_app.py
+++ app/streamlit_app.py
@@ -46,7 +46,7 @@ st.markdown("""
 </style>
 """, unsafe_allow_html=True)
 
- समेत
+ 
 # --------------------------------------------------
 # LOAD MODEL & SCALER (NO CHANGES TO FILES)
 # --------------------------------------------------
